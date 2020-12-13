2988b477eaf2d621e68d28ed5344f42401f39efe
Hussaini Maina <30973979+Housain-maina@users.noreply.github.com>
2020-12-13 10:20:42 +0100

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -8,7 +8,7 @@ session = InstaPy(username='', password='', headless_browser=True)
 with smart_run(session):
     
     following_list = session.grab_following(username="", amount="full", live_match=True, store_locally=True)
-    session.set_do_story(percentage=95, enabled=True, simulate=True)
+    session.set_do_story(percentage=100, enabled=True, simulate=True)
     session.story_by_users(following_list)    
     session.set_quota_supervisor(enabled=True,
                                  sleep_after=["likes", "follows"],
@@ -24,33 +24,33 @@ with smart_run(session):
                                  peak_unfollows_daily=400,
                                  peak_server_calls_hourly=None,
                                  peak_server_calls_daily=4700)
-    session.like_by_feed(amount=50, randomize=True, unfollow=False, interact=False)
-    session.set_delimit_liking(enabled=True, max_likes=150, min_likes=0)
-    session.set_delimit_commenting(enabled=True, max_comments=4, min_comments=0)
-    session.set_relationship_bounds(enabled=True,
+   session.like_by_feed(amount=50, randomize=True, unfollow=False, interact=False)
+   session.set_delimit_liking(enabled=True, max_likes=150, min_likes=0)
+   session.set_delimit_commenting(enabled=True, max_comments=4, min_comments=0)
+   session.set_relationship_bounds(enabled=True,
                                     potency_ratio=-0.50,
                                     delimit_by_numbers=True,
                                     max_followers=2000,
                                     max_following=3500,
                                     min_followers=25,
                                     min_following=25)
-    session.set_do_comment(False, percentage=80)
-    session.set_do_follow(enabled=True, percentage=50, times=1)
-    session.set_comments(['Amazing!', 'Awesome!!', 'Cool!', 'Good one!',
+   session.set_do_comment(False, percentage=80)
+   session.set_do_follow(enabled=True, percentage=50, times=1)
+   session.set_comments(['Amazing!', 'Awesome!!', 'Cool!', 'Good one!',
                           'Really good one', 'Love this!', 'Like it!',
                           'Beautiful!', 'Great!', 'Nice one'], media='Photo')
-    session.set_sleep_reduce(200)
-    session.unfollow_users(amount=50, instapy_followed_enabled=True,
+   session.set_sleep_reduce(200)
+   session.unfollow_users(amount=50, instapy_followed_enabled=True,
                           instapy_followed_param='nonfollowers',
                            style="FIFO",
                            unfollow_after=24 * 60 * 60,
                            sleep_delay=503)
-    session.unfollow_users(amount=50, instapy_followed_enabled=True,
+   session.unfollow_users(amount=50, instapy_followed_enabled=True,
                           instapy_followed_param='all',
                            style="FIFO",
                            unfollow_after=48 * 60 * 60,
                            sleep_delay=503)
-    session.like_by_tags(['forex','forexsignals','forexsignal','heavypips'], amount=50)
-    session.join_pods(topic='general', engagement_mode='no_comments')
+   session.like_by_tags(['forex','forexsignals','forexsignal','heavypips'], amount=50)
+   session.join_pods(topic='general', engagement_mode='no_comments')
 
 
